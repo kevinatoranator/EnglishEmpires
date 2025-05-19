7f3eb3b5c647f1fccb348f89e1844217c5e51c17
unknown <krinald@apps.fluco.org>
2025-05-19 15:24:53 -0400

    5/19

--- WebGame.js
+++ WebGame.js
@@ -5,9 +5,20 @@ var currentQuestion = -1;
 var correctAnswers = 0;
 var questions = new Array(new Question("Pick a choice", [ "a", "b", "c", "d*", "e"], 3), new Question("Pick another choice", [ "a", "b", "e*", "f"], 2));
 var components = new Array();
+var texts = new Array();
+var answers = [];
+
+const Colors = ["red", "green", "blue", "purple", "orange"];
+const GameStates = {
+	Menu: 0,
+	Quiz: 1,
+	Results: 2
+};
+var currentState;
 
 function loadGame(){
 	gameScreen.start();
+	currentState = GameStates.Menu;
 	startButton = new component(90, 30, "blue", 300, 120, "Start");
 	components.push(startButton);
 	gameScreen.canvas.addEventListener('mousemove', MouseMove);
@@ -16,6 +27,9 @@ function loadGame(){
 
 function startGame(){
 	currentQuestion = 0;
+	correctAnswers = 0;
+	answers = [];
+	currentState = GameStates.Quiz;
 	components.forEach(function(item, index){
 		if(item == startButton){
 			components.splice(index);
@@ -24,13 +38,49 @@ function startGame(){
 	gameScreen.canvas.removeEventListener("mousemove", MouseMove);
 	nextQuestion(currentQuestion);
 }
+
+function questionResults(currentQuestion, answers){
+	components = [];
+	texts = [];
+	//text with results chosen answer + right answer
+	components.push(new component(90, 30, "red", 300, 120, "Next"));
+	texts.push(new textBox(`The answer was ${currentQuestion.answers[currentQuestion.correct]}!`, ctx.canvas.width/2, 50));
+	var qanswers = new Array(currentQuestion.answers.length);
+	qanswers.fill(0);
+	answers.forEach(function(answer){
+		qanswers[currentQuestion.answers.indexOf(answer)] += 1
+	});
+	for(var i = 0; i < qanswers.length; i++){
+		texts.push(new textBox(`${currentQuestion.answers[i]} : ${qanswers[i]}`, ctx.canvas.width/2, 175 + (20 * i) , 20));
+	}
+}
+
 function nextQuestion(currentQuestion){
 	components = [];
+	texts = [];
 	components.push(questions[currentQuestion]);
 	questions[currentQuestion].answers.forEach(function(item, index){
-			answer = new component(90, 30, "red", 300, 120 + ((index + 1) * 40), item);
+			answer = new component(90, 30, Colors[index], 300, 120 + ((index + 1) * 40), item);
 			components.push(answer);
 	});
+}
+
+function results(currentQuestion){
+	texts = [];
+	currentState = GameStates.Results;
+	var menuButton = new component(90, 30, "green", 300, 120, "Menu");
+	components.push(menuButton);
+}
+
+function textBox(texty, x, y, fontSize = 40){
+	this.texty = texty;
+	this.update = function(){
+		ctx = gameScreen.context;
+		ctx.font = `${fontSize}px serif`;
+		ctx.fillStyle = "black";
+		ctx.textAlign = "center";
+		ctx.fillText(this.texty, x, y);
+	}
 }
 
 function Question(question, answers, correct){
@@ -61,29 +111,41 @@ function MouseClick(event){
 	const mousePos = getMousePos(gameScreen, event);
 		mouseX = mousePos.x;
 		mouseY = mousePos.y;
-		
-		if(isInside(mouseX, mouseY, startButton.x, startButton.y, startButton.width, startButton.height)){
-			startGame();
-		}
-		components.forEach(function(item){
-			if(isInside(mouseX, mouseY, item.x, item.y, item.width, item.height)){
-				console.log(item.text);
-				if(questions[currentQuestion].answers.indexOf(item.text) == questions[currentQuestion].correct){
-					correctAnswers++;
-				}
-				currentQuestion++;
-				if(currentQuestion < questions.length){
-					if(currentQuestion == -1){
-						startGame();
-					}else{
-						nextQuestion(currentQuestion);
-					}
-				}else{
-					console.log("Correct answers: " + correctAnswers);
-					components = [];
-				}
+		if(currentState == GameStates.Menu){
+			if(isInside(mouseX, mouseY, startButton.x, startButton.y, startButton.width, startButton.height)){
+				startGame();
 			}
-		});
+		}else if(currentState == GameStates.Quiz){
+			components.forEach(function(item){
+				if(isInside(mouseX, mouseY, item.x, item.y, item.width, item.height)){
+					console.log(item.text);
+					answers.push(item.text);
+					if(item.text == "Next"){
+						currentQuestion++;
+						if(currentQuestion < questions.length){
+							answers = [];
+							nextQuestion(currentQuestion);
+						}else{
+							console.log("Correct answers: " + correctAnswers);
+							components = [];
+							results();
+						}
+					}else{
+						if(questions[currentQuestion].answers.indexOf(item.text) == questions[currentQuestion].correct){//Correct Answer
+							correctAnswers++;
+						}
+						questionResults(questions[currentQuestion], answers);
+					}
+					
+				}
+			});
+		}else if(currentState == GameStates.Results){
+			components.forEach(function(item){
+				if(isInside(mouseX, mouseY, item.x, item.y, item.width, item.height)){
+					loadGame();
+				}
+			});
+		}
 }
 
 function getMousePos(canvas, event){
@@ -133,7 +195,17 @@ function updateGameArea(){
 	gameScreen.clear();
 	components.forEach(function(item){
 			item.update();
+			
 		});
+		
+	texts.forEach(function(texts1){
+			texts1.update()
+			
+		});
+		
+		if(currentState == GameStates.Results){
+			new textBox(`You got ${correctAnswers} questions right!`, ctx.canvas.width/2, 50).update();
+		}
 }
 	
 loadGame();
